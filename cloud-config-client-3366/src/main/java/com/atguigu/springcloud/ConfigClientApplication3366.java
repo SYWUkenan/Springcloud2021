f448628c285671cf1a5b414c5dba6a075c843b7e
SYWUkenan <251311992@qq.com>
2021-02-24 21:38:49 +0800

    新增 3366接口

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientApplication3366.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yswu
+ * @date 2021-02-24 17:16
+ */
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientApplication3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication3366.class,args);
+    }
+}
