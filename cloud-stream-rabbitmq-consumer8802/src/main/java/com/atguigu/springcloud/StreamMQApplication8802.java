34c24b9d397cc137299e23cca82fbeadc506704c
SYWUkenan <251311992@qq.com>
2021-02-25 21:57:53 +0800

    新增 消息消费者8802接口

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQApplication8802.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yswu
+ * @date 2021-02-25 21:50
+ */
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQApplication8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQApplication8802.class,args);
+    }
+}
