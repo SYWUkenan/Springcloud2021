53eca07360f113b31d3f6f47838ec9aa1ac9fa02
SYWUkenan <251311992@qq.com>
2021-02-25 22:39:03 +0800

    sleuth + zipkin链路追踪

--- cloud-provider-payment-8002/src/main/java/com/atguigu/springcloud/springcloud/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8002/src/main/java/com/atguigu/springcloud/springcloud/springcloud/controller/PaymentController.java
@@ -81,4 +81,12 @@ public class PaymentController {
     }
 
 
+
+    @GetMapping("/zipkin")
+    public String paymentZipkin()
+    {
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
+    }
+
+
 }
